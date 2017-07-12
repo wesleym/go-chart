b793388a28626be8d556c61adb143acff152d2a0
Wesley Moy <wesley.moy@gmail.com>
2017-07-12 09:51:08 -0700

    Repair minor comment typo
    This resolves the only spelling error reported by Misspell in the Go Report Card for go-chart.
    
    See https://goreportcard.com/report/github.com/wcharczuk/go-chart#misspell

--- market_hours_range.go
+++ market_hours_range.go
@@ -113,7 +113,7 @@ func (mhr MarketHoursRange) GetMarketClose() time.Time {
 }
 
 // GetTicks returns the ticks for the range.
-// This is to override the default continous ticks that would be generated for the range.
+// This is to override the default continuous ticks that would be generated for the range.
 func (mhr *MarketHoursRange) GetTicks(r Renderer, defaults Style, vf ValueFormatter) []Tick {
 	times := seq.Time.MarketHours(mhr.Min, mhr.Max, mhr.GetMarketOpen(), mhr.GetMarketClose(), mhr.GetHolidayProvider())
 	timesWidth := mhr.measureTimes(r, defaults, vf, times)
